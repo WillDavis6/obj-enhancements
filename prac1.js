48618a616f55da1e42e0cb7f5ca773ff79597f89
William <mailliwrd@gmail.com>
2023-10-25 20:18:15 -0600

    commit

---
+++ prac1.js
@@ -0,0 +1,28 @@
+const createInstructor = (firstName, lastName) => {
+    firstName,
+    lastName    
+}
+
+const instructor = (favoriteNumber, value) => {
+    [favoriteNumber], value
+    firstName, "Colt"
+}
+
+const instructors = {
+    firstName: "colt",
+    sayHi() {
+        return "Hi";
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
+};
+
+function createanimal(species, verb, noise) {
+    return {
+        species,
+        [verb]() {
+            return noise;
+        }
+    }
+}
